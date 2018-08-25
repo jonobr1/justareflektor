0a65f0e7deba893c746f7f52951fc19d37f2e607
Jono <sup@jono.fyi>
2018-08-25 11:34:35 +0200

    Updated Builds

--- sandbox/build/sandbox.js
+++ sandbox/build/sandbox.js
@@ -57289,19 +57289,19 @@ THREE.InvertShader = {
     Files: [
       new Sandbox.Asset({
         name: 'Light',
-        value: '/media/reflektor_1.mp4'
+        value: './media/reflektor_1.mp4'
       }),
       new Sandbox.Asset({
         name: 'Sea',
-        value: '/media/reflektor_3.mp4'
+        value: './media/reflektor_3.mp4'
       }),
       new Sandbox.Asset({
         name: 'Portrait',
-        value: '/media/reflektor_4.mp4'
+        value: './media/reflektor_4.mp4'
       }),
       new Sandbox.Asset({
         name: 'Multiplication',
-        value: '/media/reflektor_5.mp4'
+        value: './media/reflektor_5.mp4'
       })
     ]
 
